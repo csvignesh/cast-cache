e22a6295b307247227f8674d246e174bb2cf7c2a
vics <vics@ebay.com>
2017-06-23 03:22:45 -0700

    enabling CORS

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ client.on("ready", (obj) => console.log("ready", obj));
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/public'));
 
+// enabling CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/store', function (req, res) {
     const key = req.query.k;
     const value = req.query.v;
