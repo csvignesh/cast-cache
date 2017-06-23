89ff0a6f25290b41bdea84f22c2f8f44fec758d9
vics <vics@ebay.com>
2017-06-23 03:02:09 -0700

    client setup redis

--- index.js
+++ index.js
@@ -2,12 +2,23 @@
 
 const express = require('express');
 const app = express();
+const client = require('redis').createClient(process.env.REDIS_URL);
+
+client.on("error", (err) => console.log("Error", err));
+client.on("ready", (obj) => console.log("ready", obj));
 
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/store', function (req, res) {
-    res.send('done');
+    if (req.query.k && req.query.v) {
+        client.set(req.k, req.v, (res) => {
+            console.log(res, `store ${res.query.k} = ${res.query.v}`);
+        });
+        res.send('DONE');
+    } else {
+        res.send('params missing');
+    }
 });
 
 app.get('/get', function (req, res) {
@@ -18,6 +29,10 @@ app.get('/', function (req, res) {
     res.send('U have to ask the right question');
 });
 
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'));
+client.on("connect", (obj) => {
+    console.log("connect", obj);
+
+    app.listen(app.get('port'), function () {
+        console.log("Node app is running at localhost:" + app.get('port'));
+    });
 });
